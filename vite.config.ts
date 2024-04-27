d67b2d4cb8f521edcc8211ac168d1962f5d1ddde
Tobias Lønnerød Madsen <m@dsen.tv>
2024-04-27 11:09:01 +0200

    build: include css in build

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
   plugins: [
     vue(),
+    cssInjectedByJsPlugin(),
     dts({
       insertTypesEntry: true,
+      rollupTypes: true,
     }),
   ],
   build: {
